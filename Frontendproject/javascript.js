1b344e68f764f506c18131948f84c0f1965ff4e6
Shravana2002 <shravananaik1826@gmail.com>
2026-03-06 10:38:02 +0530

    Update javascript.js

--- Frontendproject/javascript.js
+++ Frontendproject/javascript.js
@@ -1,1 +1,29 @@
+function validateRegister(){
 
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+
+if(name == "" || email == ""){
+alert("Please fill all fields");
+return false;
+}
+
+alert("Registration Successful");
+return true;
+
+}
+
+function validateLogin(){
+
+let email = document.getElementById("loginEmail").value;
+let password = document.getElementById("loginPassword").value;
+
+if(email == "" || password == ""){
+alert("Enter Email and Password");
+return false;
+}
+
+alert("Login Successful");
+return true;
+
+}
